6982c293411cad3344953dea55b7507d2cfad885
postBG <profile2697@gmail.com>
2021-05-17 00:47:59 +0900

    update maml update

--- hw2/AML-XAI-HW2/trainer/maml.py
+++ hw2/AML-XAI-HW2/trainer/maml.py
@@ -47,52 +47,32 @@ class Trainer(trainer.GenericTrainer):
         # The components in 'results' are as follows:
         # results[0]: results for pre-update model
         # results[1:]: results for the adapted model at each inner loop step
+
+        # i: num corrects and losses after i updates
         corrects = [0 for _ in range(self.inner_step + 1)]
         losses_q = [0 for _ in range(self.inner_step + 1)]
 
         task_num, setsz, _, _, _ = x_spt.size()
         for i in range(task_num):
-            # on the first example (k=0)
-            logits = self.net(x_spt[i], vars=None, bn_training=True)
-            loss = self.loss(logits, y_spt[i])
-            grad = torch.autograd.grad(loss, self.net.parameters())
-            fast_weights = list(map(lambda p, gradient: p - self.inner_lr * gradient,
-                                    zip(self.net.parameters(), grad)))
-
             # pre-update
-            k = 0
             with torch.no_grad():
                 loss_q, correct = self._evaluate(self.net.parameters(), x_qry[i], y_qry[i])
-                losses_q[k] += loss_q
-                corrects[k] += correct
+                losses_q[0] += loss_q
+                corrects[0] += correct
 
-            # after the first update
-            k = 1
-            with torch.no_grad():
-                loss_q, correct = self._evaluate(fast_weights, x_qry[i], y_qry[i])
-                losses_q[k] += loss_q
-                corrects[k] += correct
-
-            for k in range(1, self.inner_step):
-                # 1. run the i-th task and compute loss for k=1 ~ K-1
+            fast_weights = self.net.parameters()
+            for j in range(self.inner_step):
+                # the first update
                 logits = self.net(x_spt[i], fast_weights, bn_training=True)
                 loss = self.loss(logits, y_spt[i])
-                # 2. compute grad on theta_pi
                 grad = torch.autograd.grad(loss, fast_weights)
-                # 3. theta_pi = theta_pi - train_lr * grad
                 fast_weights = list(map(lambda p, gradient: p - self.inner_lr * gradient, zip(fast_weights, grad)))
 
-                logits_q = self.net(x_qry[i], fast_weights, bn_training=True)
-                # loss_q will be overwritten and just keep the loss_q on last update step.
-                loss_q = self.loss(logits_q, y_qry[i])
-                losses_q[k + 1] += loss_q
+                loss_q, correct = self._evaluate(fast_weights, x_qry[i], y_qry[i])
+                losses_q[j + 1] += loss_q
+                corrects[j + 1] += correct
 
-                with torch.no_grad():
-                    pred_q = F.softmax(logits_q, dim=1).argmax(dim=1)
-                    correct = torch.eq(pred_q, y_qry[i]).sum().item()  # convert to numpy
-                    corrects[k + 1] = corrects[k + 1] + correct
-
-        # end of all tasks
+        # After all tasks
         # sum over all losses on query set across all tasks
         loss_q = losses_q[-1] / task_num
 
@@ -102,9 +82,9 @@ class Trainer(trainer.GenericTrainer):
         self.meta_optim.step()
 
         querysz = x_qry.size(1)
-        accs = np.array(corrects) / (querysz * task_num)
+        accuracies = np.array(corrects) / (querysz * task_num)
 
-        return accs
+        return accuracies
 
     def _evaluate(self, weights, x, y):
         logits = self.net(x, weights, bn_training=True)
