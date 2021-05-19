4acbfd9beb49d605d3e6585e330bc59b15f3d19a
postBG <profile2697@gmail.com>
2021-05-20 02:11:00 +0900

    gpu sh

---
+++ hw2/AML-XAI-HW2/gpu3.sh
@@ -0,0 +1,1 @@
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=100  --meta_lr=0.7 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1.5e-3
