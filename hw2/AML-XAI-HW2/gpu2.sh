9e86bed870c108b18adb4f8a69dc30170c11703a
postBG <profile2697@gmail.com>
2021-05-20 17:20:10 +0900

    update sh

--- hw2/AML-XAI-HW2/gpu2.sh
+++ hw2/AML-XAI-HW2/gpu2.sh
@@ -1,6 +1,6 @@
-python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1e-3
-python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1.5e-3
-python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=2e-3
-python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1e-3
-python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1.5e-3
-python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=2e-3
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=5  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1e-3
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=10  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1e-3
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=20  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1e-3
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=30  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1e-3
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=40  --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1e-3
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50 --meta_lr=1.0 --task_num=10 --inner_step=5 --batch-size=10 --inner_lr=1e-3
