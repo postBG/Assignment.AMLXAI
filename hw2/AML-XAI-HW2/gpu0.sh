78478ef9a205015c79c177c4f9f4837c8cbbd0a0
postBG <profile2697@gmail.com>
2021-05-21 20:02:32 +0900

    update configs

--- hw2/AML-XAI-HW2/gpu0.sh
+++ hw2/AML-XAI-HW2/gpu0.sh
@@ -2,4 +2,3 @@
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.1 --task_num=10 --inner_step=5  --inner_lr=1e-3
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.2 --task_num=10 --inner_step=5  --inner_lr=1e-3
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.3 --task_num=10 --inner_step=5  --inner_lr=1e-3
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.7 --task_num=10 --inner_step=5  --inner_lr=1e-3
--- hw2/AML-XAI-HW2/gpu2.sh
+++ hw2/AML-XAI-HW2/gpu2.sh
@@ -1,5 +1,3 @@
 # inner step test tuning
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=10  --meta_lr=0.5 --task_num=10 --inner_step=5  --inner_lr=1e-3
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=20  --meta_lr=0.5 --task_num=10 --inner_step=5  --inner_lr=1e-3
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=30  --meta_lr=0.5 --task_num=10 --inner_step=5  --inner_lr=1e-3
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=40  --meta_lr=0.5 --task_num=10 --inner_step=5  --inner_lr=1e-3
