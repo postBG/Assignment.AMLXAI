b7b409f03e3dc9f391a113048c75292338cc27a0
postBG <profile2697@gmail.com>
2021-05-21 15:08:12 +0900

    update config

--- hw2/AML-XAI-HW2/gpu0.sh
+++ hw2/AML-XAI-HW2/gpu0.sh
@@ -1,6 +1,5 @@
+# meta-lr tuning
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.1 --task_num=10 --inner_step=5  --inner_lr=1e-3
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.2 --task_num=10 --inner_step=5  --inner_lr=1e-3
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.3 --task_num=10 --inner_step=5  --inner_lr=1e-3
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.5 --task_num=10 --inner_step=5  --inner_lr=1e-3
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.7 --task_num=10 --inner_step=5  --inner_lr=1e-3
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=1.0 --task_num=10 --inner_step=5  --inner_lr=1e-3
