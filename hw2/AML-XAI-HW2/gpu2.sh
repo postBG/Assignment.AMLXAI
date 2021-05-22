edfbe6a49ceb34081e89974894b8fc2e40b54f88
postBG <profile2697@gmail.com>
2021-05-22 20:45:19 +0900

    update gpu0, 2

--- hw2/AML-XAI-HW2/gpu2.sh
+++ hw2/AML-XAI-HW2/gpu2.sh
@@ -1,7 +1,5 @@
 # inner step test tuning
 python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=5  --inner_lr=0.1
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=5  --inner_lr=0.1
-python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=5  --inner_lr=0.1
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=0.1 --task_num=20 --inner_step=5  --inner_lr=1e-3
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=0.1 --task_num=30 --inner_step=5  --inner_lr=1e-3
+
 
