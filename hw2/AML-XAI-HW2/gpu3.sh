6980a6661fa9c67396b4f4501991c029e6e22f1c
postBG <profile2697@gmail.com>
2021-05-23 15:29:32 +0900

    add new exp on gpu2, 3

--- hw2/AML-XAI-HW2/gpu3.sh
+++ hw2/AML-XAI-HW2/gpu3.sh
@@ -1,5 +1,4 @@
 # fomaml + inner lr tuning
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.5 --task_num=10 --inner_step=5  --inner_lr=2e-4
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.1 --task_num=10 --inner_step=5  --inner_lr=1e-4
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=0.1 --task_num=10 --inner_step=5  --inner_lr=5e-4
-python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=10  --meta_lr=1e-3 --task_num=30 --inner_step=5  --inner_lr=0.4
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=20  --inner_lr=0.01
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=20  --inner_lr=0.001
+
