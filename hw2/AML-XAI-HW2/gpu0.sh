feb64149dcfd9e09e7d3968c3e2894214f10c4f5
postBG <profile2697@gmail.com>
2021-05-24 00:58:01 +0900

    fuck it

--- hw2/AML-XAI-HW2/gpu0.sh
+++ hw2/AML-XAI-HW2/gpu0.sh
@@ -1,1 +1,1 @@
-python3 main.py --trainer maml --dataset=sine --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=10  --meta_lr=1e-3 --task_num=10 --inner_step=10  --inner_lr=5e-5 --epoch=1000
+python3 main.py --trainer maml --dataset=sine --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=10  --meta_lr=1e-3 --task_num=1000 --inner_step=1  --inner_lr=1e-4 --epoch=10000
