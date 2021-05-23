1db92b82ef5285668b74135a7371aaa892d3f844
postBG <profile2697@gmail.com>
2021-05-24 01:18:24 +0900

    fuck this assignment

--- hw2/AML-XAI-HW2/gpu1.sh
+++ hw2/AML-XAI-HW2/gpu1.sh
@@ -1,2 +1,2 @@
-python3 main.py --trainer reptile --dataset=sine --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=10  --meta_lr=1e-4 --taskgs
-_num=1000 --inner_step=10  --inner_lr=0.001 --epoch=10000
+python3 main.py --trainer reptile --dataset=sine --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=10  --meta_lr=1e-3 --task_num=1000 --inner_step=10  --inner_lr=1e-4 --epoch=10000
+python3 main.py --trainer reptile --dataset=sine --n_way=5 --k_spt=10 --k_qry=10  --inner_step_test=10  --meta_lr=1e-3 --task_num=1000 --inner_step=10  --inner_lr=1e-4 --epoch=10000
