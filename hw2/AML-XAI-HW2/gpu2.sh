c805e1faddd22a715396d89e20d8381ef93eb97e
postBG <profile2697@gmail.com>
2021-05-23 20:24:57 +0900

    fuck you TA

--- hw2/AML-XAI-HW2/gpu2.sh
+++ hw2/AML-XAI-HW2/gpu2.sh
@@ -1,4 +1,4 @@
 # inner step test tuning
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=5 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=20  --inner_lr=0.01
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=20  --inner_lr=0.01
 
 
--- hw2/AML-XAI-HW2/gpu3.sh
+++ hw2/AML-XAI-HW2/gpu3.sh
@@ -1,3 +1,4 @@
-# fomaml + inner lr tuning
-python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=50  --inner_lr=0.001
+# inner step test tuning
+python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=20  --inner_lr=0.005
+
 
