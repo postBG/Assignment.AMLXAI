dee97df6b19993f5a20af05d5d327de0d4273b85
postBG <profile2697@gmail.com>
2021-05-23 17:31:03 +0900

    update gpu 2, 3 config

--- hw2/AML-XAI-HW2/gpu3.sh
+++ hw2/AML-XAI-HW2/gpu3.sh
@@ -1,5 +1,4 @@
 # fomaml + inner lr tuning
 python3 main.py --trainer reptile --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=20  --inner_lr=0.01
 python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=10 --inner_step=5  --inner_lr=0.4
-python3 main.py --trainer fomaml --dataset=omniglot --n_way=5 --k_spt=1 --k_qry=10  --inner_step_test=50  --meta_lr=1e-3 --task_num=30 --inner_step=5  --inner_lr=0.4
 
